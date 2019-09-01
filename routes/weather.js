ab95b40d60a54bcb8afecfed680b950ed932dc2b
Çağatay <cagatay@Cagatay-MacBook-Pro.local>
2019-09-01 23:39:12 +0300

    Bug fixes

--- routes/weather.js
+++ routes/weather.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 
-router.get("/:name?", (req, res) => {
+router.get("/:name?,:country?", (req, res) => {
   request(
-    `https://api.weatherbit.io/v2.0/current?city=${req.path},tr&key=e5908d73d226497e9c8cc70d970e2f6a`,
+    `https://api.weatherbit.io/v2.0/current?city=${req.path.toUpperCase()}&key=e5908d73d226497e9c8cc70d970e2f6a`,
     (error, response, body) => {
       if (error) {
         res.send(error);
         return;
       }
       if (response.statusCode === 204) {
-        res.send("Please send city param");
+        res.send("Wrong city or country");
         return;
       }
-      res.send(response);
+      res.send(JSON.parse(response.body));
     }
   );
 });
