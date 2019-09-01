1194edc0aabfd17284d6ceca4396f378608e03cc
Çağatay <cagatay@Cagatay-MacBook-Pro.local>
2019-09-01 23:59:18 +0300

    Refactor

--- routes/weather.js
+++ routes/weather.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 
-router.get("/:name?,:country?", (req, res) => {
+const sendRequest = (path, res) => {
   request(
-    `https://api.weatherbit.io/v2.0/current?city=${req.path.toUpperCase()}&key=e5908d73d226497e9c8cc70d970e2f6a`,
+    `https://api.weatherbit.io/v2.0/current?city=${path}&key=e5908d73d226497e9c8cc70d970e2f6a`,
     (error, response, body) => {
       if (error) {
         res.send(error);
@@ -17,6 +17,16 @@ router.get("/:name?,:country?", (req, res) => {
       res.send(JSON.parse(response.body));
     }
   );
+};
+
+router.get("/:city?", (req, res) => {
+  const path = req.path.toUpperCase();
+  sendRequest(path, res);
+});
+
+router.get("/:city?,:country?", (req, res) => {
+  const path = req.path.toUpperCase();
+  sendRequest(path, res);
 });
 
 module.exports = router;
